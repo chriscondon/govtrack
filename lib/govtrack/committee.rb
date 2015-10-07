c5aeaea39c6be7defef58b9332bc07e2a4692ba3
Chris Condon <http://github.com/chriscondon>
2015-10-07 18:04:41 -0500

    Changes to Committee Module

--- lib/govtrack/committee.rb
+++ lib/govtrack/committee.rb
@@ -5,9 +5,11 @@ module GovTrack
       super
       @created = DateTime.parse(@created) if @created
     end
-    
 
     
+    def self.find_committee(id)
+      new(get("/committee/#{id}"))
+    end
 
     
   end
