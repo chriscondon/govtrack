9af739074bc38d27c62bc7c91fd802a91020dfa5
Chris Condon <chris.m.condon@gmail.com>
2015-11-29 13:15:25 -0600

    Added method to query All Committees.

--- lib/govtrack/committee.rb
+++ lib/govtrack/committee.rb
@@ -10,6 +10,12 @@ module GovTrack
     def self.find_committee(id)
       new(get("/committee/#{id}"))
     end
+    
+    # Committee Endpoint has limit of 100 items.
+    # "Offset" is used to iterate through all items by multiples of 100 (similar to a "page").
+    def self.all(offset=0)
+      new(get("/committee?offset=#{offset}"))
+    end
 
     
   end
